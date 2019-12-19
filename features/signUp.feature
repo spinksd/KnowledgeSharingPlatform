8b9afed31d423e59eeedc921ac2fac7059c5f729
Dominic Spinks <domspinks45@gmail.com>
2019-12-19 18:50:19 +0000

    Added tests for all scenarios for signUp page. Need to update the checks for whether a user was created or not - this will be done when I know how to connect to a backend database.

--- features/signUp.feature
+++ features/signUp.feature
@@ -4,10 +4,59 @@ Feature: SignUpPage
   I want to get access to the platform
   So that I can have access to useful information I can learn from
 
-  Scenario: Navigate to the sign-up page of the website when not logged in
+  Scenario: Navigate to the signup page of the platform when not logged in
 
-    Given I have the url for the sign-up page of the knowledge sharing platform website
-    And I have not logged in
+    Given I have the url for the signup page of the platform
+    And I am not logged in
     When I enter the url into my web browser
-    Then the sign-up page of the website is displayed with a form requesting my forename, surname, role, email and password
+    Then the signup page of the platform is displayed with a form requesting my forename, surname, role, email and password
 
+  Scenario: Navigate to the signup page of the platform when logged in
+
+    Given I have the url for the signup page of the platform
+    And I am logged in
+    When I enter the url into my web browser
+    Then I am directed to the search page of the platform
+
+  Scenario: Enter valid details into signup form
+
+    Given I am on the signup page of the platform
+    And I am not logged in
+    And I enter valid details into the signup form
+    When I press submit
+    Then I should have a user created with the details I entered
+    And I should be directed to the login page
+    And I should be presented with a message saying 'User succesfully created.'
+
+  Scenario: Enter invalid details into signup form
+
+    Given I am on the signup page of the platform
+    And I am not logged in
+    And I enter invalid details into the signup form
+    When I press submit
+    Then I should not have a user created
+    And I should be presented with a message saying 'Your details are invalid, please try again.'
+
+  Scenario: Enter email already in use into signup form
+
+    Given I am on the signup page of the platform
+    And I am not logged in
+    And I enter valid details into the signup form
+    And the email I have entered is already in use by an account
+    When I press submit
+    Then I should not have a duplicate user created
+    And I should be presented with a message saying 'A user has already been created for this email. Please request a new password via the 'Forgotten Password?' link.'
+
+  Scenario: Click on login button
+
+    Given I am on the signup page of the platform
+    And I am not logged in
+    When I click on the login button
+    Then I should be directed to the login page
+
+  Scenario: Click on 'forgotten password?' button
+
+   Given I am on the signup page of the platform
+   And I am not logged in
+   When I click on the 'forgotten password' button/link
+   Then I should be directed to the forgotten password page
--- features/steps/signUp.py
+++ features/steps/signUp.py
@@ -1,21 +1,66 @@
 from behave import *
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.alert import Alert
 from selenium.common.exceptions import NoSuchElementException
-import unittest
 
-@given('I have the url for the sign-up page of the knowledge sharing platform website')
-def set_url(context):
+@given('I have the url for the signup page of the platform')
+def set_signup_url(context):
     context.url = 'http://localhost:8000/signup'
 
-@given('I have not logged in')
-def check_no_user_greeting(context):
+@given('I am not logged in')
+def check_not_logged_in(context):
     try:
-        greeting_msg = context.browser.find_element_by_id('userGreeting')
+        greeting_msg = context.browser.find_element_by_name('user_greeting')
     except NoSuchElementException:
         pass
 
-@when('I enter the url into my web browser')
+@given('And I am logged in')
+def check_logged_in(context):
+    try:
+        greeting_msg = context.browser.find_element_by_name('user_greeting')
+    except NoSuchElementException:
+        raise NoSuchElementException
+
+@given('I am on the signup page of the platform')
 def go_to_signup_page(context):
+    set_signup_url()
+    go_to_page()
+
+@given('I enter valid details into the signup form')
+def enter_sign_up_form_details(context):
+    form = context.browser.find_element_by_name('signup_form')
+
+    context.browser.get_element(form, name="forename").send_keys('Joe')
+    context.browser.get_element(form, name="surname").send_keys('Bloggs')
+    context.browser.get_element(form, name="role").send_keys('Software Engineer')
+    context.browser.get_element(form, name="email").send_keys('Joe.Bloggs@example.com')
+    context.browser.get_element(form, name="password").send_keys('insecurepassword')
+     ''' Alternative implementation
+    context.browser.find_element_by_name('username_field').send_keys('Joe')
+    context.browser.find_element_by_name('surname_field').send_keys'Bloggs')
+    context.browser.find_element_by_name('role_field').send_keys('Software Engineer')
+    context.browser.find_element_by_name('email_field').send_keys('Joe.Bloggs@example.com')
+    context.browser.find_element_by_name('password_field').send_keys('insecurepassword')
+    '''
+
+@given('I enter invalid details into the signup form')
+def enter_sign_up_form_details(context):
+    form = context.browser.find_element_by_name('signup_form')
+
+    context.browser.get_element(form, name="forename").send_keys('@3139amcda?.,z')
+    context.browser.get_element(form, name="surname").send_keys('orif1=1;show databases;')
+    context.browser.get_element(form, name="role").send_keys('hacker')
+    context.browser.get_element(form, name="email").send_keys('spam.box@spam.com')
+    context.browser.get_element(form, name="password").send_keys('')
+
+@given('the email I have entered is already in use by an account')
+def duplicate_account(context):
+    submit_signup_form()
+    enter_sign_up_form_details()
+
+@when('I enter the url into my web browser')
+def go_to_page(context):
     # selenium test
     context.browser.get(context.url)
     # django internal test to check for no 404 (ensure django application is running)
@@ -23,31 +68,77 @@ def go_to_signup_page(context):
     # below will error out with message "AttributeError: 'HttpResponseNotFound' object has no attribute 'statuscode'" if 404 received (application not running)
     assert response.statuscode == 200
     
-@then('the sign-up page of the website is displayed with a form requesting my forename, surname, role, email and password')
+@when('I press submit')
+def submit_signup_form(context):
+    form = context.browser.find_element_by_name('signup_form')
+    context.get_element(form, name='submit').click()
+    #context.browser.find_element_by_name('submit_signup').click()
+
+@when('I click on the login button')
+def click_login_button(context):
+    context.browser.find_element_by_name('navbar_login').click()
+
+@when('I click on the \'forgotten password\' button/link')
+def click_forgotten_password_button(context):
+    context.browser.find_element_by_name('forgotten_password').click()
+
+@then('the signup page of the platform is displayed with a form requesting my forename, surname, role, email and password')
 def display_signup_page(context):
-    form = context.browser.find_element_by_name('signup-form')
+    form = context.browser.find_element_by_name('signup_form')
 
-    form_fields = []
-    form_fields.append(context.browser.get_element(form, name="forename"))
-    form_fields.append(context.browser.get_element(form, name="surname"))
-    form_fields.append(context.browser.get_element(form, name="role"))
-    form_fields.append(context.browser.get_element(form, name="email"))
-    form_fields.append(context.browser.get_element(form, name="password"))
+    context.browser.get_element(form, name="forename")
+    context.browser.get_element(form, name="surname")
+    context.browser.get_element(form, name="role")
+    context.browser.get_element(form, name="email")
+    context.browser.get_element(form, name="password")
 
-    for field in form_fields:
-        assert field is not None 
+@then('I am directed to the search page of the platform')
+def check_directed_to_search_page(context):
+    assert context.browser.current_url == 'http://localhost:8000/search'
+
+@then('I should have a user created with the details I entered')
+def confirm_user_created(context):
+    assert False
+    '''
+    Fill in with connection to mysql backend
+    '''
+
+@then('I should be directed to the login page')
+def check_directed_to_login_page(context):
+    assert context.browser.current_url == 'http://localhost:8000/login'
     
+@then('I should be presented with a message saying \'User succesfully created.\'')
+def check_user_creation_message(context):
+    alert_text = Alert(context.browser).text
+    #alert = context.browser.find_element_by_name('alert')   
+    assert alert_text == 'User successfully created.'
 
-'''
-@given('we have behave installed')
-def step_impl(context):
-    pass
+@then('I should have a user created')
+def confirm_user_not_created(context):
+    assert False
+    '''
+    Fill in with connection to mysql backend
+    '''
 
-@when('we implement a test')
-def step_impl(context):
-    assert True is not False
+@then('I should be presented with a message saying \'Your details are invalid, please try again.\'')
+def check_user_creation_message(context):
+    alert_text = Alert(context.browser).text
+    #alert = context.browser.find_element_by_name('alert')   
+    assert alert_text == 'Your details are invalid, please try again.'
 
-@then('behave will test it for us!')
-def step_impl(context):
-    assert context.failed is False
-'''
+@then('I should not have a duplicate user created')
+def confirm_no_duplicate_user(context):
+    assert False
+    '''
+    Fill in with connection to mysql backend
+    '''
+
+@then('I should be presented with a message saying \'A user has already been created for this email. Please request a new password via the \'Forgotten Password?\' link.\'')
+def check_duplicate_email_message(context):
+    alert_text = Alert(context.browser).text
+    #alert = context.browser.find_element_by_name('alert')   
+    assert alert_text == 'A user has already been created for this email. Please request a new password via the \'Forgotten Password?\' link.'
+
+@then('I should be directed to the forgotten password page')
+def check_directed_to_forgotten_password_page(context):
+    assert context.browser.current_url == 'http://localhost:8000/forgotten_password'
