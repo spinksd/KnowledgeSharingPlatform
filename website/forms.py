9274421604ebdcfe1b9b1bdfe1c5e62f3ef6f339
Dominic Spinks <domspinks45@gmail.com>
2020-02-12 20:11:53 +0000

    Added logic to limit number of contacts to 6 when submitting page form

--- website/forms.py
+++ website/forms.py
@@ -23,9 +23,16 @@ class CreateUpdatePageForm(forms.ModelForm):
         }
     
     def clean_tags(self):
+        # Ensure no more than 6 tags have been added to page
         tags = self.cleaned_data.get('tags', [])
         if len(tags) > 6:
             raise forms.ValidationError('Invalid number of tags. Can\'t have more than 6 tags!', code='invalid')
+
+        # Ensure no more than 6 co tacts have been added to page
+        contacts = self.cleaned_data.get('contacts', [])
+        if len(contacts) > 6:
+            raise forms.ValidationError('Invalid number of contacts. Can\'t have more than 6 contacts!', code='invalid')
+
         # The below should return the data of the current form
         # Such that the user's submitted data remains in the form (however, this currently doesn't seem to work)
         return self.cleaned_data
